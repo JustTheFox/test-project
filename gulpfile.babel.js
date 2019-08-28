a35f76e85f95aa86ffc851f8392e94df3baa31f4
justthefox <spiridonova73l@gmail.com>
2019-08-28 14:08:38 +0400

    fix packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -31,25 +31,6 @@ const requireDir = require("require-dir"),
                 "./src/js/**/*.js"
             ]
         },
-        images: {
-            src: [
-                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
-                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
-            ],
-            dist: "./dist/img/",
-            watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg}"
-        },
-        webp: {
-            src: [
-                "./src/img/**/*.{jpg,jpeg,png,tiff}",
-                "!./src/img/favicon/*.{jpg,jpeg,png,gif}"
-            ],
-            dist: "./dist/img/",
-            watch: [
-                "./src/img/**/*.{jpg,jpeg,png,tiff}",
-                "!./src/img/favicon.{jpg,jpeg,png,gif}"
-            ]
-        },
         sprites: {
             src: "./src/img/svg/*.svg",
             dist: "./dist/img/sprites/",
@@ -74,11 +55,11 @@ requireDir("./gulp-tasks/");
 
 export { paths };
 
-export const development = gulp.series("clean", "smart-grid",
-    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons"]),
+export const development = gulp.series("clean",
+    gulp.parallel(["views", "styles", "scripts", "sprites", "fonts", "favicons"]),
     gulp.parallel("serve"));
 
 export const prod = gulp.series("clean",
-    gulp.series(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons", "gzip"]));
+    gulp.series(["views", "styles", "scripts", "sprites", "fonts", "favicons", "gzip"]));
 
 export default development;
