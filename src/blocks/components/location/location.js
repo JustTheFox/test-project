0ef4a23629fb8e05808807661b8e6832661bad84
justthefox <spiridonova73l@gmail.com>
2019-08-28 15:48:59 +0400

    add components

---
+++ src/blocks/components/location/location.js
@@ -0,0 +1,13 @@
+modules.define('location', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
