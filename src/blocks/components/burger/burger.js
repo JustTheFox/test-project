7c804e5392b36573eaf1a89f6ae2c0c85184244a
justthefox <spiridonova73l@gmail.com>
2019-08-28 17:44:15 +0400

    header and footer style

--- src/blocks/components/burger/burger.js
+++ src/blocks/components/burger/burger.js
@@ -4,7 +4,13 @@ provide(bemDom.declBlock(this.name, {
     onSetMod: {
         js: {
             inited: function() {
-                
+                var $burgerLink = $('.js-open-menu');
+
+                if ($burgerLink.length && $burgerCont.length) {
+                    $burgerLink.on('click', function (e) {
+                        $burgerLink.toggleClass('open');
+                    });
+                }
             }
         }
     }
