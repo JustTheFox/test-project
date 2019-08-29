8d409b2fda257173a7b57f9c540c31046813ce61
justthefox <spiridonova73l@gmail.com>
2019-08-30 01:02:20 +0400

    fix overflow

--- src/blocks/components/slider/slider.js
+++ src/blocks/components/slider/slider.js
@@ -5,7 +5,7 @@ var $slider = $(".js-slider");
 var $prevArrow = "<button href='javascript:void(0);' class='arrow arrow--prev'><svg class=\"icon icon-arrow-left\"><use xlink:href=\"img/sprites/sprite.svg#arrow-left\"/></svg></button>";
 var $nextArrow = "<button href='javascript:void(0);' class='arrow arrow--next'><svg class=\"icon icon-arrow-right\"><use xlink:href=\"img/sprites/sprite.svg#arrow-right\"/></svg></button>";
 var responsive = function () {
-    if (window.innerWidth >= 124 && !$slider.hasClass("slick-initialized")) {
+    if (window.innerWidth >= 1024 && !$slider.hasClass("slick-initialized")) {
         $slider.slick({
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -20,10 +20,21 @@ var responsive = function () {
         $slider.slick("unslick");
     }
 };
+
+// var $sliderHeight = $("html").css("height");
+//     console.log($sliderHeight);
+// var itemHeight = function () {
+//     $(".slider__item").height($sliderHeight);
+// };
+
+//itemHeight();
+
 if ($slider.length > 0) {
     responsive();
+    //itemHeight();
 
     $(window).resize(function () {
         responsive();
+        //itemHeight();
     });
 }
