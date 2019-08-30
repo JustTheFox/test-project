47b32362502676c76c9a8d648b6a6c79b7a60394
justthefox <spiridonova73l@gmail.com>
2019-08-30 11:35:03 +0400

    fix closing menu on ESC

--- src/blocks/components/burger/burger.js
+++ src/blocks/components/burger/burger.js
@@ -1,34 +1,24 @@
 import $ from "jquery";
 
-var $burgerLink = $(".js-open-menu");
-var $burgerMenu = $(".js-menu");
+$(document).ready(function(){
+    var $burgerLink = $(".js-open-menu");
+    var $burgerMenu = $(".js-menu");
 
-$burgerLink.on("click", function () {
-    $burgerLink.toggleClass("active");
-    $burgerMenu.toggleClass("active");
+    $burgerLink.on("click", function () {
+        $burgerLink.toggleClass("active");
+        $burgerMenu.toggleClass("active");
 
-    if (!$burgerMenu.hasClass("active")) {
-        $burgerMenu.fadeOut( "300ms" );
-    } else {
-        $burgerMenu.fadeIn( "300ms" );
-    }
+        if (!$burgerMenu.hasClass("active")) {
+            $burgerMenu.fadeOut( "300ms" );
+        } else {
+            $burgerMenu.fadeIn( "300ms" );
+        }
+    });
+    $(this).keydown(function(e) {
+        if (e.keyCode == 27) {
+            $burgerLink.removeClass("active");
+            $burgerMenu.removeClass("active");
+            $burgerMenu.fadeOut( "300ms" );
+        }
+    });
 });
-
-// modules.define("burger", ["i-bem-dom"], function(provide, bemDom) {
-
-//     provide(bemDom.declBlock(this.name, {
-//         onSetMod: {
-//             js: {
-//                 inited: function() {
-//                     var $burgerLink = $("js-open-menu");
-//                     console.log("kek");
-//                     $burgerLink.on("click", function () {
-//                         console.log("lol");
-//                         $burgerLink.toggleClass("active");
-//                     });
-//                 }
-//             }
-//         }
-//     }));
-
-// });
